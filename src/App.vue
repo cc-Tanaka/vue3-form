e969400b3104745eeee8002389d6ef86729fd87a
cc-Tanaka <tanaka-ryo@colorfulcompany.co.jp>
2021-01-05 09:43:51 +0900

    modalを実装

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <router-link to="/form_01">フォーム画面へ</router-link>
   <router-view></router-view>
+  <Modal/>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 
 export default {
   data() {
     return {
     }
+  },
+  components: {
+    Modal
   }
 }
 </script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <button @click="openModal">open</button>
+    <div class="overlay" v-show="showModal" @click="closeModal">
+      <div class="modal">
+        <p>Hello Modal</p>
+        <button @click="closeModal">close</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  data: function() {
+    return {
+      showModal: false
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true
+    },
+    closeModal() {
+      this.showModal = false
+    }
+  }
+};
+</script>
+<style>
+.overlay {
+  z-index: 1;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color:rgba(0,0,0,0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  z-index: 2;
+  width: 400px;
+  height: 320px;
+  padding: 16px;
+  background: #fff;
+  border-radius: 8px;
+}
+</style>
